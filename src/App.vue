a5022d41b10fa284e0777dc24b9dd3fc6b6da0d8
Sergiy Poddubka <poseal@gmail.com>
2021-08-18 19:14:33 +0300

    add buttons with methods

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <div>
-            <button>Like</button>
-            <button>Dislike</button>
+            <button v-on:click="addLike">Like</button>
+            <button @click="addDislike">Dislike</button>
         </div>
         <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
         <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
@@ -16,6 +16,14 @@ export default {
       likes: 0,
       dislikes: 0
     };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    }
   }
 };
 </script>
