0c526e9d6788642628d0a817c00fcfa332b4e80c
Sergiy Poddubka <poseal@gmail.com>
2021-08-18 19:01:56 +0300

    add App component and simple template

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <button>Like</button>
+            <button>Dislike</button>
+        </div>
+        <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
+        <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0
+    };
+  }
+};
+</script>
+
+<style>
+</style>
