65a74d1a6345da376d9db2b28dc2fa9d6fe776ca
Sergiy Poddubka <poseal@gmail.com>
2021-08-23 14:28:03 +0300

    add routing

---
+++ src/pages/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Данное приложение было создано для обучения Vue 3</h1>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Добро пожаловать на проект Vue 3</h1>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
