0c798dc6f865bdb05dc00dd5fc93e25716dcd307
Sergiy Poddubka <poseal@gmail.com>
2021-08-23 14:53:25 +0300

    add navbar with routing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
+    <navbar></navbar>
     <div class="app">
       <router-view></router-view>
     </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+export default {
+  components: { Navbar }
+};
 </script>
 
 <style>
