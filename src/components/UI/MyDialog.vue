59ba7a3b229f7544e16a974473b42849dd090e86
Sergiy Poddubka <poseal@gmail.com>
2021-09-01 16:43:34 +0300

    add mixin

--- src/components/UI/MyDialog.vue
+++ src/components/UI/MyDialog.vue
@@ -7,19 +7,13 @@
 </template>
 
 <script>
+import toggleMixin from "@/mixins/toggleMixin";
+
 export default {
   name: "my-dialog",
-  props: {
-    // видимость окна
-    show: {
-      type: Boolean,
-      default: false
-    }
-  },
-  methods: {
-    hideDialog() {
-      this.$emit("update:show", false);
-    }
+  mixins: [toggleMixin],
+  mounted() {
+    console.log("dialog mounted");
   }
 };
 </script>
